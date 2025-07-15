70051d23bd0e2299747f526d922412b8fda97ff8
Jackymn25 <jacky.huo@mail.utoronto.ca>
2025-07-15 15:36:04 -0400

    Add starter code(not for credit part)
    starter code cited: 
    https://github.com/HeyuanZ621/homework-5-group   forked from  https://github.com/CSC207-2025S-UofT/homework-5-group
    Note: previous work in git log of https://github.com/HeyuanZ621/homework-5-group, 10 commits ahead https://github.com/CSC207-2025S-UofT/homework-5-group

---
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -0,0 +1,16 @@
+package use_case.logout;
+
+/**
+ * The Input Data for the Logout Use Case.
+ */
+public class LogoutInputData {
+    private final String username;
+
+    public LogoutInputData(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
---
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -0,0 +1,24 @@
+package use_case.logout;
+
+/**
+ * The Logout Interactor.
+ */
+public class LogoutInteractor implements LogoutInputBoundary {
+    private LogoutUserDataAccessInterface userDataAccessObject;
+    private LogoutOutputBoundary logoutPresenter;
+
+    public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
+                            LogoutOutputBoundary logoutOutputBoundary) {
+        this.userDataAccessObject = userDataAccessInterface;
+        this.logoutPresenter = logoutOutputBoundary;
+    }
+
+    @Override
+    public void execute(LogoutInputData logoutInputData) {
+        userDataAccessObject.setCurrentUsername(null);
+        final LogoutOutputData newdata = new LogoutOutputData(logoutInputData.getUsername(),
+                false);
+        logoutPresenter.prepareSuccessView(newdata);
+    }
+}
+
