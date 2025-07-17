ecdd523a332235dce0289ed6eb6f004ea658e8c7
Jacky Huo <jacky.huo@mail.utoronto.ca>
2025-07-16 23:38:54 -0400

    Bug fix

--- src/main/java/use_case/login/LoginInteractor.java
+++ src/main/java/use_case/login/LoginInteractor.java
@@ -37,4 +37,9 @@ public class LoginInteractor implements LoginInputBoundary {
             }
         }
     }
+
+    @Override
+    public void switchToSignUpView() {
+        loginPresenter.switchToSignUpView();
+    }
 }
