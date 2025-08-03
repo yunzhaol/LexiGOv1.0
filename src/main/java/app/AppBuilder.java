fc0eb2bf83f321a8a950c74a1729e78991a15f63
Jacky Huo <jacky.huo@mail.utoronto.ca>
2025-08-02 21:53:25 -0400

    App UI name renamed

--- src/main/java/app/AppBuilder.java
+++ src/main/java/app/AppBuilder.java
@@ -380,7 +380,7 @@ public class AppBuilder {
      * @return the application
      */
     public JFrame build() {
-        final JFrame application = new JFrame("Login Example");
+        final JFrame application = new JFrame("LexiGo App");
         application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         application.add(cardPanel);
