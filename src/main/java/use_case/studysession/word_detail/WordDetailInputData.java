500530d6fa74c1c950d3368c5e730a112e9a83a3
周禾原 <zhouheyuan@Heyuans-MacBook-Air.local>
2025-08-05 20:44:50 -0400

    checkstyle study sesion

--- src/main/java/use_case/studysession/word_detail/WordDetailInputData.java
+++ src/main/java/use_case/studysession/word_detail/WordDetailInputData.java
@@ -1,6 +1,6 @@
 package use_case.studysession.word_detail;
 
-public class WordDetailInputData {
+public final class WordDetailInputData {
     private String currpage;
 
     public WordDetailInputData(String currpage) {
